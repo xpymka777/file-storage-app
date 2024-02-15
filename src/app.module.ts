d3c3a3820bd90441c94114111a108460f52b74de
xpymka777 <gumangu12@gmail.com>
2024-02-15 16:02:25 +0700

    + cors политика(вроде как)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 
@@ -12,8 +12,10 @@ import { FolderController } from './folder/folder.controller';
 import { FolderService } from './folder/folder.service';
 import { FileController } from './file/file.controller';
 import { FileService } from './file/file.service';
+
 @Module({
   imports: [
+    //конфиг для подключения к бд
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -21,16 +23,42 @@ import { FileService } from './file/file.service';
       username: 'postgres',
       password: 'root',
       database: 'file-service',
-      entities: ['dist/**/*.entity{.ts,.js}'],
+      entities: [UserEntity, FolderEntity, FileEntity],
       synchronize: true,
     }),
     TypeOrmModule.forFeature([UserEntity, FolderEntity, FileEntity]),
+    //для jwt токена устанавливаем ключ генерации и время жизни
     JwtModule.register({
       secret: '123321',
       signOptions: { expiresIn: '1h' },
     }),
   ],
+  //подключаем контроллеры и сервисы для работы в приложении
   controllers: [UserController, FolderController, FileController],
   providers: [UserService, FolderService, FileService],
 })
-export class AppModule {}
+export class AppModule {
+  // Метод configure позволяет добавить middleware для обработки CORS запросов
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      // Применяем middleware Express для обработки CORS запросов
+      .apply((req, res, next) => {
+        // Устанавливаем заголовки для разрешения CORS
+        res.header('Access-Control-Allow-Origin', '*'); // Разрешаем доступ с любого домена
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept', // Указываем разрешенные заголовки
+        );
+        if (req.method === 'OPTIONS') {
+          // Если метод OPTIONS, то возвращаем разрешенные методы
+          res.header(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, DELETE',
+          );
+          return res.status(200).json({}); // Возвращаем успешный статус
+        }
+        next(); // Передаем управление следующему middleware
+      })
+      .forRoutes({ path: '*', method: RequestMethod.ALL }); // Обрабатываем CORS запросы для всех маршрутов и методов
+  }
+}
